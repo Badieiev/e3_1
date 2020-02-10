19f8813184ea0bea1da018dc34576bbcf9bbf486
Andrii Badieiev <andrey64freemail@gmail.com>
2020-02-10 23:19:07 +0200

    11pm

---
+++ e3_1/Controllers/ListHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace e3_1.Controllers
+{
+    public static class ListHelper
+    {
+        public static MvcHtmlString CreateOrderedList(this HtmlHelper html, List<string> items)
+        {
+            var ol = new TagBuilder("ol");
+            foreach (var item in items)
+            {
+                var li = new TagBuilder("li");
+                li.SetInnerText(item);
+                ol.InnerHtml += li.ToString();
+            }
+            return new MvcHtmlString(ol.ToString());
+        }
+    }
+}
